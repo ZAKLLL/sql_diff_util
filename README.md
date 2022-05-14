6ff1dd0288168995a4a2be9ec41bfd769ac4127d
Zaklll <1015849735@qq.com>
2022-05-14 10:26:52 +0800

    完善输出效果，完善readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# MYSQL8_DB_DDL_DIFF
-一个实现数据库 表字段 diff的脚本实现
+# MYSQL_DB_DDL_DIFF
+一个实现mysql 数据表字段 diff的脚本实现
+可用于快速对比测试环境与正式环境数据库ddl差异
+
+# 使用方式
+1. 安装pymysql(如果没有安装的话):
+    ```pip install pymysql```
+    
+2. 更改main函数中的db地址:
+    ```python3
+    if __name__ == '__main__':
+
+    db1 = '127.0.0.1:3306/sc_auth?user=root&password=ZJK15626933724'
+    db2 = '127.0.0.1:3306/sc_auth2?user=root&password=ZJK15626933724'
+    ret = diffDbDDL(db1, db2)
+    ```
+    
+3. 执行应用：
+    ``` python -u sqldiff.py```
+
+4. 查看输出:
+
+    ![输出](img1.png)
+
--- sqldiff.py
+++ sqldiff.py
@@ -53,15 +53,17 @@ def diffDbDDL(dbaddr1, dbaddr2):
     # 处理表是否存在问题
     tableNotExistInB = list(tablesa-tablesb)
     tableNotExistInA = list(tablesb-tablesa)
-    print("-------------------------table if exist-------------------------\n")
+    print("\033[0;32;40m -------------------------------------table if exist-------------------------------------\033[0m \n\n")
+    tmp="""tables %s :
+                            exist in %s
+                            not   in %s\n\n"""
+    tmp=tmp.replace('%s','\033[1;31;40m%s\033[0m')
     if tableNotExistInA:
-
-        tmp = 'tables: %s exist in %s not exist in %s \n'
         print(tmp % (tableNotExistInA, dbNameB, dbNameA))
     if tableNotExistInB:
         print(tmp % (tableNotExistInB, dbNameA, dbNameB))
 
-    print("-------------------------table ddl diff-------------------------\n")
+    print("\n\033[0;32;40m -------------------------------------table ddl diff-------------------------------------\033[0m \n")
 
     tableDiffData = {}
     for table in ddlA:
@@ -74,38 +76,48 @@ def diffDbDDL(dbaddr1, dbaddr2):
         tableAName = dbNameA+'.'+table
         tableBName = dbNameB+'.'+table
         for field in d1:
+            fieldExistTmp="""field %s :
+                                    exist in %s
+                                    not   in %s"""
+            fieldExistTmp=fieldExistTmp.replace('%s','\033[1;31;40m%s\033[0m')
             if field not in d2:
-                diffPoints.append('field: %s exist in %s not in %s' %
+                diffPoints.append(fieldExistTmp %
                                   (field, tableAName, tableBName))
                 continue
             p1, p2 = d1[field], d2[field]
+            tmp ="""field %s %s :
+                                %s in %s
+                                %s in %s"""
+            tmp=tmp.replace('%s','\033[1;31;40m%s\033[0m')
             if p1.type != p2.type:
                 diffPoints.append(
-                    'field: %s type:  %s in %s ; %s in %s  ' % (field, p1.type, tableAName, p2.type, tableBName))
+                    tmp % (field, 'type', p1.type, tableAName, p2.type, tableBName))
             if p1.null != p2.null:
                 diffPoints.append(
-                    'field: %s null:  %s in %s ; %s in %s  ' % (field, p1.null, tableAName, p2.null, tableBName))
+                    tmp % (field, 'null', p1.null, tableAName, p2.null, tableBName))
             if p1.default != p2.default:
                 diffPoints.append(
-                    'field: %s default:  %s in %s ; %s in %s  ' % (field, p1.default, tableAName, p2.default, tableBName))
+                    tmp % (field, 'default', p1.default, tableAName, p2.default, tableBName))
 
         for field in d2:
             if field not in d1:
-                diffPoints.append('field: %s exist in %s not in %s' %
+                diffPoints.append(fieldExistTmp %
                                   (field, tableBName, tableAName))
                 continue
 
     for i in tableDiffData:
+        if not tableDiffData[i]:
+            continue
         print('table-------------->', i)
         for j in tableDiffData[i]:
-            print('\t\t\t', j, '\n')
+            print('\t\t', j, '\n')
 
     return tableDiffData
 
 
 if __name__ == '__main__':
 
-    db1 = '127.0.0.1:3306/world?user=root&password=123456'
-    db2 = '127.0.0.1:3306/world2?user=root&password=123456'
+    db1 = '127.0.0.1:3306/sc_auth?user=root&password=ZJK15626933724'
+    db2 = '127.0.0.1:3306/sc_auth2?user=root&password=ZJK15626933724'
     ret = diffDbDDL(db1, db2)
     # print(ret)
